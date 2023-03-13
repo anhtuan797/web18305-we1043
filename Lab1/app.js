377f172f58724ab6a017a30077bc3d18c0524fc3
Tuan <tuanpapd08217@fpt.edu.vn>
2023-03-13 12:35:10 +0700

    nap lab

---
+++ Lab1/app.js
@@ -0,0 +1,7 @@
+let a = prompt("Moi ban nhap so nguyen a: ");
+//let a1 = parseInt(a);
+let b = prompt(" Moi ban nhap vao so nguyen b: ");
+//let b1 = parseInt(b);
+let c = parseInt(a) + parseInt(b);
+//let c = a + b;
+document.write(c);
